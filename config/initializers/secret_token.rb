c1d3683eb5ffe209e8a80574c09300888c1546d8
Kiy Hong Tang <kiyhong@gmail.com>
2014-04-19 10:36:22 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Amazon::Application.config.secret_key_base = 'fec75d8ce1fd6976d5aa28a6d7e844cb8cd95310f9588e0936342c3f300c1e8a5549d57870bee357bace3fba98f944cd38ea8bca0282a921e6f788dfa04e0cfa'
