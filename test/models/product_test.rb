5cc99a3f13576f8de68a5db7abfedecba13b5838
Kiy Hong Tang <kiyhong@gmail.com>
2014-04-20 11:37:32 +0100

    Added test data to the fixture file

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures  :products
   test "product attributes must not be empty" do
     product = Product.new
     assert product.invalid?
@@ -28,4 +29,41 @@ class ProductTest < ActiveSupport::TestCase
     product.price = 1
     assert product.valid?
    end
+
+  def new_product(image_url)
+    Product.new(title:       "My Book Title", 
+                description: "yyy", 
+                price:        1, 
+                image_url:    image_url) 
+  end
+  test "image url" do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg 
+              http://a.b.c/x/y/z/fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+    ok.each do |name|
+      assert new_product(name).valid?, "#{name} should be valid"
+    end
+    bad.each do |name|
+      assert new_product(name).invalid?, "#{name} should't be valid"
+    end
+  end  
 end
+
+#   def new_product(image_url)
+#     Product.new(title:       "My Book Title",
+#                 description: "yyy",
+#                 price:       1,
+#                 image_url:   image_url)
+#   end
+#   test "image url" do
+#     ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
+#              http://a.b.c/x/y/z/fred.gif }
+#     bad = %w{ fred.doc fred.gif/more fred.gif.more }
+#     ok.each do |name|
+#       assert new_product(name).valid?, "#{name} should be valid"
+#     end
+#     bad.each do |name|
+#       assert new_product(name).invalid?, "#{name} shouldn't be valid"
+#     end
+#   end
+# end
