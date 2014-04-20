184866e919421f2cfea65b4fccd455da8d8952b6
Kiy Hong Tang <kiyhong@gmail.com>
2014-04-20 13:27:57 +0100

    Added :lenght validation to title

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ActiveRecord::Base
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
+  validates :title, length: { minimum: 10 } #check title is at least 10characters long
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\Z}i,
     message: 'must be a URL for GIF, JPG or PNG image.'  
