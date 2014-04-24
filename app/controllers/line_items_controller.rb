77df0841fc9c2a2d0d86420cf2b26f9f409ae9fb
Kiy Hong Tang <kiyhong@gmail.com>
2014-04-24 15:50:06 +0100

    Allow reset of counter to zero when utem is added to cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -26,9 +26,10 @@ class LineItemsController < ApplicationController
   # POST /line_items
   # POST /line_items.json
   def create
+    session[:counter] = 0
     product = Product.find(params[:product_id])
     @line_item = @cart.line_items.build(product: product)
-
+ 
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
